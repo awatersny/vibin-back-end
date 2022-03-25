ce32b8ce9b57bef01bde69e9932ffba90849bcdb
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-25 11:36:53 -0400

    fix typo

--- models/profile.js
+++ models/profile.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: {type: String, required: true},
-  avatar: {type:String, default: 'blank-avata (insert default avatar here'},
+  avatar: {type:String, default: 'blank-avatar (insert default avatar here'},
   instruments: String, //[...Strings] ,
   // bands: (Objectid ref:"bands"),
   // reviews: (ObjectIds ref:'reviews'),
