08a1dee25861c49305fa62ef0065f7f94fac2b2b
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-25 13:49:50 -0400

    add status to profile model.

--- models/profile.js
+++ models/profile.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose'
 
 const profileSchema = new mongoose.Schema({
-  email: {type: String, required: true, lowercase: true, unique: true},
-  name: {type: String, required: true},
+  email: {type: String, required: true, lowercase: true, unique: true },
+  name: {type: String, required: true },
   avatar: {type:String, default: 'blank-avatar (insert default avatar here'},
   instruments: String, //[...Strings] ,
   // bands: (Objectid ref:"bands"),
   // reviews: (ObjectIds ref:'reviews'),
   zip: {type: Number, required: true },
+  status: {type: String, required: true },
   // bselected: [ObjectIds ref: 'band'],
   // brejected: [ObjectIds ref: 'band'],
 
