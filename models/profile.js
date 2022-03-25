9a8a670f0f7b75a14725701d22a33f2755a17051
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-25 10:58:23 -0400

    build out the login models so we can add more detail to the login page.

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  avatar: String, 
+  instruments: String, //[...Strings],
+  // bands: (Objectid ref:"bands"),
+  // reviews: (ObjectIds ref:'reviews'),
+  zip: Number,
+  // bselected: [ObjectIds ref: 'band'],
+  // brejected: [ObjectIds ref: 'band'],
+
 },{
     timestamps: true,
 })
