cf28de209d74645ba362cfc682713c83ebe2e563
awatersny <awatersny@gmail.com>
2022-03-27 18:43:32 -0400

    Add routes and controllers for bands entity

--- routes/bands.js
+++ routes/bands.js
@@ -5,11 +5,16 @@ import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
+// router.get('/', bandsCtrl.index)
+// router.post('/', bandsCtrl.create)
+// router.put('/:id', bandsCtrl.update)
+// router.delete('/:id', bandsCtrl.delete)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, bandsCtrl.index)
 router.post('/', checkAuth, bandsCtrl.create)
+router.put('/:id', checkAuth, bandsCtrl.update)
+router.delete('/:id', checkAuth, bandsCtrl.delete)
 
 export { router }
