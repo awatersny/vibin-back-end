a5c6966ee4a0200ef887cf717461b551a204a599
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-29 13:13:07 -0400

    add bio to profile model

--- models/profile.js
+++ models/profile.js
@@ -10,6 +10,7 @@ const profileSchema = new Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   photo: {type: String, default: 'blank-avatar (insert default avatar here'},
   role: {type: Number, min: 100, max: 900, default: 100},
+  bio: {type: String },
   genres: [{type: Schema.Types.ObjectId, ref:"Genre"}],
   // instruments: [{type: Schema.Types.ObjectId, ref:"Instrument"}],
   bands: [{type: Schema.Types.ObjectId, ref:"Band"}],
