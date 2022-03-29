bba61ef1567ec349fe5944daafb4e74af66c1bf3
DVNJNKNS <devonmjenkins3@gmail.com>
2022-03-29 13:08:27 -0400

    change avatar to photo and comment out isntruments again, becuase they're still causing a fetch error.

--- models/profile.js
+++ models/profile.js
@@ -8,7 +8,7 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, lowercase: true, unique: true},
-  avatar: {type: String, default: 'blank-avatar (insert default avatar here'},
+  photo: {type: String, default: 'blank-avatar (insert default avatar here'},
   role: {type: Number, min: 100, max: 900, default: 100},
   genres: [{type: Schema.Types.ObjectId, ref:"Genre"}],
   // instruments: [{type: Schema.Types.ObjectId, ref:"Instrument"}],
